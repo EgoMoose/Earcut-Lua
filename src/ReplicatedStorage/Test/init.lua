67393911678b38103b329302dd29e692621180bb
EgoMoose <EgoMoose@users.noreply.github.com>
2020-09-27 22:46:15 -0400

    Add earcut
    Initial port of earcut. Still need to run a number of test cases to verify.

--- src/ReplicatedStorage/Test/init.lua
+++ src/ReplicatedStorage/Test/init.lua
@@ -1,3 +1,7 @@
-print("Hello world!")
+local Earcut = require(game.ReplicatedStorage.Earcut).Earcut
+
+local triangles = Earcut({0,0, 100,0, 100,100, 0,100,  20,20, 80,20, 80,80, 20,80}, {4})
+
+print(table.concat(triangles, ", "))
 
 return true
